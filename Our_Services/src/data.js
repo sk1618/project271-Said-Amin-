066c844238d3d2d1b778617ecce17a8f8c6316f4
sk1618 <sak79@mail.aub.edu>
2025-04-09 16:43:40 +0300

    Create data.js

---
+++ Our_Services/src/data.js
@@ -0,0 +1,40 @@
+import smartExpense from "./images/smartExpense.png";
+import future from "./images/future.png";
+import budget from "./images/budget.png";
+import custom from "./images/custom.png";
+import assistance from "./images/assistance.png";
+import secure from "./images/secure.png";
+
+
+export default[
+    {
+        img: smartExpense,
+        name: "Smart Expense Tracking",
+        des: "Easily record and categorize all your business expenses in real-time.",
+    },
+    {
+        img: future,
+        name: "Future Purchase Planning",
+        des: "Organize your upcoming purchases with our interactive To-Buy list.",
+    },
+    {
+        img: budget,
+        name: "Budget Insights & Analytics",
+        des: "Visualize your spending habits and optimize your financial decisions.",
+    },
+    {
+        img: custom,
+        name: "Custom Expense Categories",
+        des: "Tailor categories to fit your business model and workflow.",
+    },
+    {
+        img: assistance,
+        name: "AI-powered Chatbot Assistance",
+        des: "Get real-time help with managing your expenses, purchases, and budgeting from our intelligent chatbot.",
+    },
+    {
+        img: secure,
+        name: "Secure Cloud Backup",
+        des: "Safeguard your financial data with encrypted cloud storage.",
+    }
+];
